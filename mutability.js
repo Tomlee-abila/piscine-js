83330b20f195bfdf97d858b7341e2cc252ee63e2
Tomlee Abila <tomleeabila@gmai.com>
2024-08-26 13:29:27 +0300

    update

--- mutability.js
+++ mutability.js
@@ -1,9 +1,3 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-}
-  
 const clone1 = Object.assign().person
 const clone2 = Object.assign().person
 const samePerson = person
@@ -11,8 +5,5 @@ const samePerson = person
 Object.freeze(clone1)
 Object.freeze(clone2)
 
-
-
 person.age += 1
 person.country = 'FR'
-console.log(clone1)
