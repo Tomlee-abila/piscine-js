54738d8ca753f91f82746473de6100595af5f021
Tomlee Abila <tomleeabila@gmai.com>
2024-08-26 13:32:27 +0300

    update

--- mutability.js
+++ mutability.js
@@ -1,5 +1,5 @@
-const clone1 = Object.assign().person
-const clone2 = Object.assign().person
+const clone1 = Object.assign({}.person)
+const clone2 = Object.assign({}.person)
 const samePerson = person
 
 Object.freeze(clone1)
