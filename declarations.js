180f729387b36f70f67d0534626fbf3b59d47ad2
Tomlee Abila <tomleeabila@gmai.com>
2024-08-26 12:31:46 +0300

    feat: decLaring const arr, obj and nested then freezing them  so that they can not be changed

---
+++ declarations.js
@@ -0,0 +1,22 @@
+const escapeStr = "'\\/\"."
+const arr = [4,2]
+const obj = {
+    str: "Hello",
+    num: 10,
+    bool: true,
+    undef: undefined
+}
+
+const nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: "Hello",
+        num: 10,
+        bool: true,
+        undef: undefined
+    }
+}
+
+Object.freeze(obj);
+Object.freeze(nested);
+Object.freeze(arr);
