e2635324c860a48a3eac7bbd5f6fd0dea2a6d889
Tomlee Abila <tomleeabila@gmai.com>
2024-08-28 17:36:50 +0300

    update

--- method-man.js
+++ method-man.js
@@ -1,1 +1,7 @@
 const words = (str) => str.split(" ")
+const sentence = (arr) => arr.join(" ")
+const yell = (str) => str.toUpperCase()
+const whisper = (str) => str.toLowerCase()
+const capitalize = (str) => str.charAt(0)+str.slice(1).toLowerCase()
+
+console.log(capitalize('HELLO'))
