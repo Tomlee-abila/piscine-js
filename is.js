fa2675c02b62c6c60c246074ca1805cc26095614
Tomlee Abila <tomleeabila@gmai.com>
2024-08-28 19:15:33 +0300

    update

--- is.js
+++ is.js
@@ -6,7 +6,7 @@ is.bool = (n) => typeof n == 'boolean' ? true : false
 is.undef = (n) => typeof n == 'undefined' ? true : false
 is.def = (n) => typeof n !== 'undefined' ? true : false
 is.arr = (n) => Array.isArray(n) ? true : false
-is.obj = (n) => typeof n == 'object' && !(n instanceof Date || n instanceof Map || n instanceof Set || Array.isArray(n) || n instanceof RegExp) 
+is.obj = (n) => typeof n == 'object' && !(n instanceof Date || n instanceof Map || n instanceof Set || Array.isArray(n) || n instanceof RegExp) ? true : false
 is.fun = (n) => typeof n == 'function' ? true : false
 is.truthy = (n) => (n) ? true : false
 is.falsy = (n) => !(n) ? true : false
