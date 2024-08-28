794452819d3655cf9c7ff12457b7736df9811ea3
Tomlee Abila <tomleeabila@gmai.com>
2024-08-29 00:28:00 +0300

    update

---
+++ physics.js
@@ -0,0 +1,13 @@
+function getAcceleration(obj){
+    if (typeof obj.f === 'number' && typeof obj.m === 'number'){
+        return obj.f/obj.m
+    }
+
+    if (typeof obj.Δv === 'number' && typeof obj.Δt === 'number'){
+        return obj.Δv/obj.Δt
+    }
+
+    if (typeof obj.d === 'number' && typeof obj.t === 'number'){
+        return (obj.d*2)/(obj.t**2)
+    }
+}
