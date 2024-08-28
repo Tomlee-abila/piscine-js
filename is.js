13c1bdaf9d1f5b0fd2ee71b01d01ec002a9b7a36
Tomlee Abila <tomleeabila@gmai.com>
2024-08-28 19:03:20 +0300

    update

--- is.js
+++ is.js
@@ -1,13 +1,12 @@
-const is = {
-    num: (n) => typeof n == 'number' ? true : false,
-    nan: (n) => typeof n == 'number' ? true : false,
-    str: (n) => typeof n == 'string' ? true : false,
-    bool: (n) => typeof n == 'boolean' ? true : false,
-    undef: (n) => typeof n == 'undefined' ? true : false,
-    def: (n) => typeof n !== 'undefined' ? true : false,
-    arr: (n) => Array.isArray(n) ? true : false,
-    obj: (n) => typeof n == 'object' ? true : false,
-    fun: (n) => typeof n == 'function' ? true : false,
-    truthy: (n) => (n) ? true : false,
-    falsy: (n) => !(n) ? true : false
-}
+const is = {}
+is.num = (n) => typeof n == 'number' ? true : false
+is.nan = (n) => typeof n == 'number' ? true : false
+is.str = (n) => typeof n == 'string' ? true : false
+is.bool = (n) => typeof n == 'boolean' ? true : false
+is.undef = (n) => typeof n == 'undefined' ? true : false
+is.def = (n) => typeof n !== 'undefined' ? true : false
+is.arr = (n) => Array.isArray(n) ? true : false
+is.obj = (n) => typeof n == 'object' ? true : false
+is.fun = (n) => typeof n == 'function' ? true : false
+is.truthy = (n) => (n) ? true : false
+is.falsy = (n) => !(n) ? true : false
