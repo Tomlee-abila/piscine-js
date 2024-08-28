89e6a86a83dedcd443cf01f689b28a27aedbdb28
Tomlee Abila <tomleeabila@gmai.com>
2024-08-29 00:08:49 +0300

    update

--- is.js
+++ is.js
@@ -1,12 +1,12 @@
-// const is = {}
-is.num = (n) => typeof n == 'number' ? true : false
+const is = {}
+is.num = (n) => typeof n == 'number'
 is.nan = (n) => Number.isNaN(n)
-is.str = (n) => typeof n == 'string' ? true : false
-is.bool = (n) => typeof n == 'boolean' ? true : false
-is.undef = (n) => typeof n == 'undefined' ? true : false
-is.def = (n) => typeof n !== 'undefined' ? true : false
-is.arr = (n) => Array.isArray(n) ? true : false
-is.obj = (n) => typeof n == 'object' && !(n instanceof Date || n instanceof Map || n instanceof Set || Array.isArray(n) || n instanceof RegExp) ? true : false
+is.str = (n) => typeof n == 'string'
+is.bool = (n) => typeof n == 'boolean'
+is.undef = (n) => typeof n == 'undefined'
+is.def = (n) => typeof n !== 'undefined'
+is.arr = (n) => Array.isArray(n)
+is.obj = (n) => typeof n == 'object' && !(n instanceof Date || n instanceof Map || n instanceof Set || Array.isArray(n) || n instanceof RegExp || n === null)
 is.fun = (n) => typeof n == 'function' ? true : false
 is.truthy = (n) => (n) ? true : false
 is.falsy = (n) => !(n) ? true : false
