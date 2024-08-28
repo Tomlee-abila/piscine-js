5b37b41e888b634536bb6d9c7c8e5e9bfed38b9d
Tomlee Abila <tomleeabila@gmai.com>
2024-08-28 18:06:12 +0300

    update

--- abs.js
+++ abs.js
@@ -1,7 +1,12 @@
-const isPositive = (n) => !(n < 0)
+const isPositive = (n) => (n > 0)
 function abs (n){
     if (isPositive(n)){
         return n
     }
+    if (n == 0){
+        return 0
+    }
     return n*-1
 }
+
+console.log(abs(-5))
