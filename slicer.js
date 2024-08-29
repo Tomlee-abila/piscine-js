d1b1db1d521c784a9076cf2fd75baa3668e33a04
Tomlee Abila <tomleeabila@gmai.com>
2024-08-29 15:09:32 +0300

    update

--- slicer.js
+++ slicer.js
@@ -2,6 +2,9 @@ function slice(input, start, opEnd = input.length){
     if (start < 0){
         start = input.length + start
     }
+    if (opEnd < 0){
+        opEnd = input.length + opEnd
+    }
     if (Array.isArray(input)){
         let result = []
         for (let i = start; i < opEnd; i++){
@@ -26,4 +29,4 @@ function slice(input, start, opEnd = input.length){
     
 }
 
-console.log(slice('abcdef', 0, 2))
+console.log(slice(["hello", "there", "sharon", "how", "have", "you", "been"], -2))
