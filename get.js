994f73f1b5654a26e7277b037e53917ce7028887
Tomlee Abila <tomleeabila@gmai.com>
2024-08-30 22:58:09 +0300

    update

---
+++ get.js
@@ -0,0 +1,18 @@
+function get(src, path){
+    let arr = path.split(".")
+    let result
+    if (arr.length > 1){
+        src = src[arr[0]]
+        result = get(src, arr.slice(1).join("."))
+    }else{
+        return src[arr[0]]
+    }
+    return result
+}
+
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key'
+
+
+// console.log(get(src,"hello.there"))
+console.log(get(src, path))
