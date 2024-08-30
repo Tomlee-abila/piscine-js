e374987513ffe0af7f4107553de71557e621b0c2
Tomlee Abila <tomleeabila@gmai.com>
2024-08-31 00:16:37 +0300

    update

---
+++ pyramid.js
@@ -0,0 +1,22 @@
+const pyramid = (str, num) => {
+    let result = ""
+    let count = 0
+    for (let i = num; i >= 1; i--){
+        for (let j= 1; j <= i; j++){
+            result += " "            
+        }
+        for (let k = num-i; k >= 0; k--){
+            result += str            
+        }
+        for (let l = count; l > 0; l--){
+            result += str
+        }        
+        if (i !== 1){
+            result += "\n"
+        } 
+        count++       
+    }
+    return result
+}
+
+console.log(pyramid("*", 5))
