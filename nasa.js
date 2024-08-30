3b02843d2084d747b789f927f3cbe18602c1f950
Tomlee Abila <tomleeabila@gmai.com>
2024-08-30 16:23:07 +0300

    update

--- nasa.js
+++ nasa.js
@@ -1,15 +1,15 @@
 function nasa(n){
     let result = ""
 
-    for (let i = 0; i <= n; i++){
-        if (i%3 == 0 && i !== 0){
+    for (let i = 1; i <= n; i++){
+        if (i%3 == 0){
             if (i%5 == 0){
                 result += " NASA"
             }else{
                 result += " NA"
             }
             
-        }else if (i%5 == 0 && i !== 0){
+        }else if (i%5 == 0){
             result += " SA"
         }else{
             if (i == 0){
