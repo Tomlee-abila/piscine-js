a2fa62d1cd06a8fdba4cf2ffa0e514419444e366
Tomlee Abila <tomleeabila@gmai.com>
2024-08-31 12:15:46 +0300

    update

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,8 @@
 const letterSpaceNumber = (str) =>{
-    const reg = /([a-zA-Z_])[ ]([0-9][^\d])/g
+    const reg = /([a-zA-Z_])[ ](\d{1})/g
     const myArray = str.match(reg);
     return myArray
 }
 // console.log('example 1, \p{Z}example 20')
 console.log(letterSpaceNumber('example 1, example 20'))
+console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
