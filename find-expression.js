810f4641e07e01a0da865b25d280fde520b6fb84
Tomlee Abila <tomleeabila@gmai.com>
2024-08-31 04:00:16 +0300

    update

---
+++ find-expression.js
@@ -0,0 +1,36 @@
+const add4 = '+4'
+const mul2 = '*2'
+
+
+const findExpression = (num)=>{
+    if (num < 1){
+        return undefined
+    }
+
+    function recursive(current, operation){
+
+        if (current > num){
+            return undefined
+        }
+
+        if (current === num){
+            return operation
+        }
+
+        let result = recursive(current + 4, operation + " " + add4)
+        if (result !== undefined){
+            return result
+        }
+
+        result = recursive(current * 2, operation + " " + mul2)
+        if (result !== undefined){
+            return result
+        }
+        
+        return undefined
+    }
+
+    return recursive(1, "1")
+}
+
+console.log(findExpression(102))
