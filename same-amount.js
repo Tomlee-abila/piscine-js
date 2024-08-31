40caa860afb58a97127d75145efb09b67f495307
Tomlee Abila <tomleeabila@gmai.com>
2024-08-31 14:29:18 +0300

    update

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,7 @@
 function sameAmount(str, reg1, reg2){
-    return str.match(new RegExp(reg1, "g")).length === str.match(new RegExp(reg2, "g")).length
+    reg1 = new RegExp(reg1, "g")
+    reg2 = new RegExp(reg2, "g")    
+    return str.match(reg1).length == str.match(reg2).length
 }
+
+// console.log(!sameAmount("hello there, where are you", /h/, /w/))
