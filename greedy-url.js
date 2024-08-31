ca2cf5d92b6135e836337e92477d39cad1c089ea
Tomlee Abila <tomleeabila@gmai.com>
2024-08-31 16:17:37 +0300

    update

--- greedy-url.js
+++ greedy-url.js
@@ -1,14 +1,14 @@
 const getURL = (str) =>{
-    const reg = /(https?:\/\/)[\w.\/]+/g
+    const reg = /(https?:\/\/)[\w.\/\S]+/g
     let result = str.match(reg)
     return result
 }
 
 const greedyQuery = (str) =>{
-    const reg = /(https?:\/\/\w.\/\?\w\=\w\&+)/g
+    const reg = /(https?:\/\/)[\w.\/\S]+/g
     let result = str.match(reg)
     return result
 }
 
 const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you"
-console.log(getURL(dataSet))
+console.log(greedyQuery(dataSet))
