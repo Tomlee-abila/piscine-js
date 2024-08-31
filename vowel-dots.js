26772fa94c7a39fe9be2204d3c71d3cc759e5c39
Tomlee Abila <tomleeabila@gmai.com>
2024-08-31 13:36:38 +0300

    update

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,7 +1,8 @@
 const vowelDots = (str) =>{
     console.log(str)
      const reg = /(a|A|e|E|i|I|o|O|u|U)/g
-    return str.replace(reg, "$&.")
+     console.log(str.replace(reg, "$&."))
+    return str.replace(reg, "$&.")    
 }
 
-// console.log(vowelDots("Hello there"))
+console.log(vowelDots("Hello there"))
