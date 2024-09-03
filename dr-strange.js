40274a9c6b05c8fff285a606d2eeced30535f959
Tomlee Abila <tomleeabila@gmai.com>
2024-09-04 01:20:51 +0300

    update

--- dr-strange.js
+++ dr-strange.js
@@ -20,15 +20,16 @@ function addWeek(Date){
     ]
     var actual_day = Date.getDay()   
     var m_value = parseInt(Date.toString().match(reg)[0])%14
-    if (m_value > 7){
+    if (m_value > 7 || m_value == 0){
         return arr2[actual_day]
     }
     // console.log(actual_day)
     return arr1[actual_day]
 }
 
-console.log(addWeek(new Date('0001-01-31')))
-console.log(addWeek(new Date('0001-01-06')) === 'Saturday')
+console.log(new Date('Fri May 29 2020 21:22:22 GMT+0100 (Western European Summer Time)'))
+console.log(new Date('2020-05-29 23:25:22'))
+console.log(addWeek(new Date('0001-01-14')) === 'secondSunday')
 
 function timeTravel(object){
 
