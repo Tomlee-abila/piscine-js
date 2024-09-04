d8e180cf09aed454dc4b9c1db4e36430e8f0e29e
Tomlee Abila <tomleeabila@gmai.com>
2024-09-04 09:45:40 +0300

    update

--- get-some-time.js
+++ get-some-time.js
@@ -3,17 +3,29 @@ function firstDayWeek(wNumber, year) {
     if (wNumber < 1 || wNumber > 53) {
         return 'Invalid week number';
     }
+    const millisecondsPerSecond = 1000;
+    const secondsPerMinute = 60;
+    const minutesPerHour = 60;
+    const hoursPerDay = 24;
+    const millisecondsPerDay = millisecondsPerSecond * secondsPerMinute * minutesPerHour * hoursPerDay;
+
 
     
     var weekOneDate = new Date(`${year}-01-01`);
-
+    console.log("Original Date:",weekOneDate.toString())
     
     var dayOfWeek = weekOneDate.getDay(); 
     var daysToAdd = (1 - dayOfWeek + 7) % 7;
     var firstMonday = new Date(weekOneDate.setDate(weekOneDate.getDate() + daysToAdd));
-    
-    var dateOfWeek = new Date(firstMonday);
-    dateOfWeek.setDate(firstMonday.getDate() + (wNumber - 1) * 7);
+    console.log(" first monday:", firstMonday.toString())
+    var sub = 1
+    if (Number(year) < 1970){
+        sub = 2
+    }
+    var add = (wNumber - sub) * 7 * millisecondsPerDay
+    var dateOfWeek = new Date(firstMonday.getTime() + add);
+    console.log("       Answer:",dateOfWeek.toString())
+
     if (wNumber == 1){
         dateOfWeek = new Date(`${year}-01-01`);
     }
@@ -22,9 +34,10 @@ function firstDayWeek(wNumber, year) {
     var month = String(dateOfWeek.getMonth() + 1).padStart(2, '0');
     var yearFormatted = String(dateOfWeek.getFullYear()).padStart(4, '0');
     
-    console.log(dateOfWeek)
     return `${day}-${month}-${yearFormatted}`;
 }
 
 
-console.log(firstDayWeek(2, '0001')); 
+console.log(firstDayWeek(52, '1000')); 
+
+
