0ff92b7aa01ebedc7bf630c81a543ca600b4328d
Tomlee Abila <tomleeabila@gmai.com>
2024-09-04 10:55:17 +0300

    update

--- match-cron.js
+++ match-cron.js
@@ -4,8 +4,9 @@ function matchCron(cronString, date) {
     const minute = date.getUTCMinutes();
     const hour = date.getUTCHours();
     const dayOfMonth = date.getUTCDate();
-    const month = date.getUTCMonth() + 1; 
+    const month = date.getUTCMonth() + 2; 
     const dayOfWeek = (date.getUTCDay() + 1) % 7 || 7; 
+    console.log(month)
     
     
     const matchesPattern = (pattern, value) => {
@@ -20,3 +21,5 @@ function matchCron(cronString, date) {
            matchesPattern(cronDayOfWeek, dayOfWeek);
 }
 
+
+// console.log(matchCron('* * * * 1', new Date('2020-12-01 00:00:00'))); 
