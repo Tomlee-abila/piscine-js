623a6ae0d410321110ac61fd999f5341ee87ad06
Tomlee Abila <tomleeabila@gmai.com>
2024-09-04 06:59:25 +0300

    update

--- get-some-time.js
+++ get-some-time.js
@@ -6,23 +6,27 @@ function firstDayWeek(wNumber, year) {
 
     
     var weekOneDate = new Date(`${year}-01-01`);
+    console.log(weekOneDate.toString())
 
     
     var dayOfWeek = weekOneDate.getDay(); 
     var daysToAdd = (1 - dayOfWeek + 7) % 7;
     var firstMonday = new Date(weekOneDate.setDate(weekOneDate.getDate() + daysToAdd));
-
+    console.log(firstMonday.toString())
     
     var dateOfWeek = new Date(firstMonday);
     dateOfWeek.setDate(firstMonday.getDate() + (wNumber - 1) * 7);
-
+    if (wNumber == 1){
+        dateOfWeek = weekOneDate
+    }
     
     var day = String(dateOfWeek.getDate()).padStart(2, '0');
     var month = String(dateOfWeek.getMonth() + 1).padStart(2, '0');
     var yearFormatted = dateOfWeek.getFullYear();
+    
 
     return `${day}-${month}-${yearFormatted}`;
 }
 
 
-console.log(firstDayWeek(10, 2013)); 
+console.log(firstDayWeek(1, '1000')); 
