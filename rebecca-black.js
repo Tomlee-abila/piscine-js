3e821e47ca42c3cfab85023b82239ae7f7c26776
Tomlee Abila <tomleeabila@gmai.com>
2024-09-04 10:32:37 +0300

    update

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,5 +1,5 @@
 const isFriday = (date) => date.getDay() == 5
-const isWeekend = (date) => (date.getDay() == 7 || date.getDay() == 0)
+const isWeekend = (date) => (date.getDay() == 6 || date.getDay() == 0)
 const isLeapYear = (date) => date.getFullYear()%4 == 0
 const isLastDayOfMonth = (date) => {
     var onday = 86400000
