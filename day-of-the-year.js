3bbf6793e65a77f10ba6e71fa957cccdcf1dfa82
Tomlee Abila <tomleeabila@gmai.com>
2024-09-04 21:29:59 +0300

    update

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,13 @@
 function dayOfTheYear(date){
     var year = date.getFullYear()
-    var newDate = new Date(`${year}-01-01`)
+    const yearStr = year.toString().padStart(4, '0');
+    var month = date.getMonth()
+    var day = date.getDay()
+    console.log("day:",day,"\nmonth:",month,"\nyear:",year)
+
+    var newDate = new Date(`${yearStr}-01-01`)
+    console.log(newDate.toString())
+    console.log(date.toString())
     var count = 1
     while (!(date.toString() === newDate.toString())){
         count++
@@ -8,3 +15,5 @@ function dayOfTheYear(date){
     }
     return count
 }
+
+console.log(dayOfTheYear(new Date("0001-01-01")))
