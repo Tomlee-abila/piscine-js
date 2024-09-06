48cf8d813bcb9f2df5d3b6ef50a8087d326da40b
Tomlee Abila <tomleeabila@gmai.com>
2024-09-06 05:22:55 +0300

    update

--- reduce.js
+++ reduce.js
@@ -24,6 +24,10 @@ const reduce = (arr, func, accumulator = undefined) =>{
 }
 
 const reduceRight = (arr, func, accumulator = undefined) =>{
+    if (arr.length === 0) {
+        return accumulator
+    }
+    
     for (let i=arr.length-1; i>=0; i--){
         if(accumulator == undefined && i == arr[arr.length-1]){
             accumulator = arr[i]
