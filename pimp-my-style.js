0d0f4fe7864abe82f7b74ceddd7f15fb482d006c
Tomlee Abila <tomleeabila@gmai.com>
2024-09-08 05:20:45 +0300

    update

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,24 +1,20 @@
-import {styles} from "./pimp-my-style.data.js"
-let counter = 0
-let rev = false
-export const pimp = () => {
-    let element = document.getElementsByTagName("button")[0];    
-    
-    if (element.className == "button"){
-        element.classList.add(styles[0])
-        // rev = false 
-        return
+import { styles } from "./pimp-my-style.data.js";
+let counter = 0;
+export function pimp() {
+  let element = document.getElementsByTagName("button")[0];
+  if (!element.classList.contains("unpimp")) {
+    element.classList.add(styles[counter]);
+    counter++;
+
+  } else {
+    counter--;
+    element.classList.remove(styles[counter]);
+
+    if (counter === 0) {
+        element.classList.toggle("unpimp");
     }
-    if (rev){
-        element.classList.remove(styles[counter-1])
-        counter--
-        return
-    }
-    if (counter < styles.length){
-        element.classList.add(styles[counter])
-    }else{
-        rev = true
-    }    
-    console.log(element.classList)
-    counter++
+  }
+  if (counter === styles.length) {
+    element.classList.toggle("unpimp");
+  }
 }
