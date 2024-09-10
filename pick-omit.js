b9bbc3ef6abdbff0fbffcf39cff5f2fe904f4314
Tomlee Abila <tomleeabila@gmai.com>
2024-09-10 15:06:34 +0300

    update

--- pick-omit.js
+++ pick-omit.js
@@ -1,2 +1,11 @@
-const pick = (obj, arrStr) => Object.fromEntries(obj).filter(([key, value]) => Array.isArray(arrStr)?arrStr.contains(key): arrStr == key)
-const omit = (obj, arrStr) => Object.fromEntries(obj).filter(([key, value]) => Array.isArray(arrStr)? !(arrStr.contains(key)): arrStr != key)
+const pick = (obj, arrStr) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => Array.isArray(arrStr)?arrStr.includes(key): arrStr == key))
+const omit = (obj, arrStr) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => Array.isArray(arrStr)? !(arrStr.includes(key)): arrStr != key))
+
+const ob = {
+    "name": "Abila",
+    "firstName" : "Tomlee",
+    "last":24,
+    "lastName": "Otieno"
+}
+
+console.log(omit(ob, ['firstName', 'lastName']))
