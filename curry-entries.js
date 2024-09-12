7685a7a27009c4e185884f873c31409436bdaa67
Tomlee Abila <tomleeabila@gmai.com>
2024-09-12 06:32:54 +0300

    currying

---
+++ curry-entries.js
@@ -0,0 +1,92 @@
+const defaultCurry = (obj) => (obj1) =>{
+    let result = {...obj}
+    let arr = Object.entries(obj1)
+
+    arr.forEach(([key, value]) =>{
+        if (result[key] !== undefined){
+            result[key] = value
+        }
+    })
+    return result
+}
+
+//console.log(defaultCurry({
+//     http: 403,
+//     connection: 'close',
+//     contentType: 'multipart/form-data',
+//   })({
+//     http: 200,
+//     connection: 'open',
+//     requestMethod: 'GET'
+//   })
+//   )
+
+const mapCurry = (func) => (obj) =>{
+    let result = {}
+
+    Object.entries(obj).forEach(([key, value])=>{
+        let answer = func([key, value])
+        result[answer[0]] = answer[1]
+    })
+    return result
+}
+
+// const personnel = {
+//     lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+//     sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+//     zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+//     ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+//     calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+//   }
+//console.log(mapCurry(([k, v]) => [`${k}_force`, v])(personnel))
+
+
+const reduceCurry = (func) => (obj, initialValue) =>{
+    let accumulator = initialValue;
+    const entries = Object.entries(obj);
+    
+    entries.forEach(([key, value]) => {
+      accumulator = func(accumulator, [key, value]);
+    })
+    
+    return accumulator;
+}
+
+//console.log(reduceCurry((acc, [k, v]) => (acc += v))({ a: 1, b: 2, c: 3 }, 0))
+
+const filterCurry = (func) =>(obj) =>{
+    let result = {}
+    Object.entries(obj).forEach(([key, value])=>{
+        if (func(([key, value]))){
+            result[key] = value
+        }
+    })
+    return result
+}
+
+//console.log(filterCurry(([k, v]) => typeof v === 'string' || k === 'arr')({
+//     str: 'string',
+//     nbr: 1,
+//     arr: [1, 2],
+//   }))
+
+const reduceScore =(obj, accu = 0)=>{
+    let accumulator = accu
+    Object.entries(obj).forEach((object) => accumulator += reduceCurry((acc, [key, value]) => (key == "pilotingScore" || key == "shootingScore")?(acc += value):acc)(object[1], 0))
+    return accumulator
+}
+//console.log(reduceScore(personnel))
+
+const filterForce = (obj) => filterCurry(([key, value]) => value.shootingScore >= 80)(obj)
+//console.log(filterForce(personnel))
+
+const mapAverage = (obj) => {
+    
+    return mapCurry(([key, value]) => {
+        
+        let avarage = (value.pilotingScore + value.shootingScore)
+        value.averageScore = avarage
+        return [key, value]
+     })(obj)
+}
+//console.log(mapAverage(personnel))
