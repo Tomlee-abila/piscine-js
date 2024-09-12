59f9a1040ff9d82a88d6fe92f4c886da38ee4e31
Tomlee Abila <tomleeabila@gmai.com>
2024-09-12 10:01:55 +0300

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,42 +1,55 @@
 const deepCopy = (obj) =>{
+    if (obj == undefined || obj == null || (obj.constructor !== Array || obj.constructor !== Object)){
+        return obj
+    }
     if (obj.constructor == Array){
         let result = []
         obj.forEach(arr => {
             let arrR
-            switch(arr.constructor){                
-                case Array:
-                    arrR = deepCopy(arr)
-                   break
-                case Object:
-                    arrR = deepCopy(arr)
-                    break
-                default:
-                    arrR = arr
+            if (arr == undefined){
+                result.push(arr)
+            }else{
+                switch(arr.constructor){                
+                    case Array:
+                        arrR = deepCopy(arr)
+                       break
+                    case Object:
+                        arrR = deepCopy(arr)
+                        break
+                    default:
+                        arrR = arr
+                }
+                result.push(arrR)
             }
-            result.push(arrR)
+            
         })
         return result
     }else{
         let result = {}
         let arr = Object.entries(obj)
+        
         arr.forEach(([key, value]) => {
             let valuer = value
-            switch(value.constructor){
-                case Array:
-                   valuer = deepCopy(value)
-                   break
-                case Object:
+            if (value == undefined){
+                result[key] = valuer
+            }else{
+                switch(value.constructor){
+                    case Array:
                     valuer = deepCopy(value)
                     break
-                default:
-                    valuer = value
+                    case Object:
+                        valuer = deepCopy(value)
+                        break
+                    default:
+                        valuer = value
+                }
+                result[key] = valuer
             }
-            result[key] = valuer
+            
         })
         return result
     }
 }
-
 // console.log(deepCopy({2:3,4:5,7:6}))
 // const r = Math.random()
 //   const obj = [r, Object.freeze([r, Object.freeze([r])])]
@@ -45,3 +58,4 @@ const deepCopy = (obj) =>{
 //   console.log(obj)
 // //   eq(copy, obj)
 //   console.log(obj[1][1] === obj[1][1])
+// console.log(deepCopy(2))
