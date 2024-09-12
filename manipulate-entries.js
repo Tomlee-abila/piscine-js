0609411de68db234de61bed18ac793fb43aa5b4b
Tomlee Abila <tomleeabila@gmai.com>
2024-09-12 03:35:59 +0300

    Feat: add lowcarbs and carttotal

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -79,18 +79,18 @@ const totalCalories = obj =>{
 } 
 
 const lowCarbs = obj => filterValues(obj, (key, value) => ((value/100)*nutritionDB[key].carbs) < 50)
-const cartTotal = obj => mapValues(filterKeys(nutritionDB, (key, value) => obj[key] !== undefined), ([key, value]) => mapValues(value, ([key2, value2])=> parseFloat(value2*(obj[key]/100)).toFixed(1)))
-// const nutritionDB = {
-//     tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
-//     vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
-//     oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
-//     onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
-//     garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
-//     paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
-//     sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
-//     orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
-// }
-// const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
+const cartTotal = obj => mapValues(filterKeys(nutritionDB, (key, value) => obj[key] !== undefined), ([key, value]) => mapValues(value, ([key2, value2])=> Number(parseFloat(value2*(obj[key]/100)).toFixed(1))))
+const nutritionDB = {
+    tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+    vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+    oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+    onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+    garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+    sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+    orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+}
+const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
 
-// console.log(totalCalories(groceriesCart))
+console.log(cartTotal(groceriesCart))
   
