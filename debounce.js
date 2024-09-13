f49735d7a9d7d9b6e1050a9a39aaf45c4035a431
Tomlee Abila <tomleeabila@gmai.com>
2024-09-13 23:05:23 +0300

    debounce

--- debounce.js
+++ debounce.js
@@ -12,9 +12,10 @@ const debounce = (func, delay = 1000) =>{
 
 const opDebounce = (func, delay = 1000, option = {leading: false, trailing: true}) =>{
     let timeout
+    let trigered = false
 
     return (...args)=>{
-        if (option.leading) func(...args)
+        if (option.leading && !trigered) func(...args), trigered = true
 
         if (option.trailing){
             clearTimeout(timeout)
