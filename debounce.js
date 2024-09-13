f6354f5236fb366ee892f100840ba59dfb78a1d5
Tomlee Abila <tomleeabila@gmai.com>
2024-09-13 14:47:18 +0300

    debounce

---
+++ debounce.js
@@ -0,0 +1,12 @@
+const debounce = (func, delay = 1000) =>{
+    let timeout
+
+    return (...args)=>{
+        clearTimeout(timeout)
+
+        timeout = setTimeout(() =>{
+            func(...args)
+        }, delay)
+    }
+}
+
