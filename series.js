11c8a621c5931b163001bdd914eb75ecb5fd28a7
Tomlee Abila <tomleeabila@gmai.com>
2024-09-15 11:02:16 +0300

    update

--- series.js
+++ series.js
@@ -1,12 +1,22 @@
-const all = async (obj)=>{
-    let keys = Object.keys(obj)
-    if (obj == {})return obj
-    let result = []
-    for (let i = 0; i < keys.length; i++){        
-        result.push(await obj[keys[i]]())
-    }
-    return result
-}
+// const all = async (obj)=>{
+//     let keys = Object.keys(obj)
+//     if (obj == {})return obj
+//     let result = []
+//     for (let i = 0; i < keys.length; i++){        
+//         result.push(await obj[keys[i]]())
+//     }
+//     return result
+// }
 
-console.log(all({hello: ()=> "hello"}))
-console.log(all({}))
+// // console.log(all({hello: ()=> "hello"}))
+// // console.log(all({}))
+
+async function series(args) {
+    let results = [];
+    for (const fn of args) {
+      const result = await fn(); 
+      results.push(result); 
+    }
+    return results;
+  }
+  
