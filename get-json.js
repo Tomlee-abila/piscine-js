51500f52b5db9ed713b9acd2b4cf21bd783f3cce
Tomlee Abila <tomleeabila@gmai.com>
2024-09-15 05:59:35 +0300

    update

---
+++ get-json.js
@@ -0,0 +1,21 @@
+async function getJSON  (path, params = {}) {
+    const queryString = new URLSearchParams(params).toString();
+    const url = queryString ? `${path}?${queryString}` : path;
+
+    try{
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        const data = await response.json();
+
+        if (data.error) {
+            throw new Error(data.error);
+        }
+        return data;
+    }catch(error){
+        throw error;
+    }
+}
