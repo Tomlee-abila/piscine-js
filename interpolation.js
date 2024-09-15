a4e8af008cd5f57793feaad2e841e87a1b659563
Tomlee Abila <tomleeabila@gmai.com>
2024-09-15 03:21:28 +0300

    update

--- interpolation.js
+++ interpolation.js
@@ -1,18 +1,29 @@
 function interpolation({ step, start, end, callback, duration }) {
     const interval = duration / step;
-    const delta = end - start;
+    const delta = (end - start)/step;
     let currentStep = 0;
+    let d = start
 
     const intervalId = setInterval(() => {
-        const t = currentStep / (step - 1);
-        const point = start + t * delta;
+        const t = d;
+        start += interval
+        
 
-        callback([t, point]);
-
+        callback([t, start]);
+        d += delta
         currentStep++;
+        
 
-        if (currentStep >= step) {
+        if (start >= duration) {
             clearInterval(intervalId);
         }
     }, interval);
 }
+
+// interpolation({
+//     step: 5,
+//     start: 0,
+//     end: 1,
+//     callback: ([x, y]) => console.log(`[${x.toFixed(2)}, ${y.toFixed(2)}]`),
+//     duration: 10
+// });
