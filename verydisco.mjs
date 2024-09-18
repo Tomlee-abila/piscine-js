70f8f3170c18e50860737280d59a8bb9ba442e79
Tomlee Abila <tomleeabila@gmai.com>
2024-09-18 15:29:45 +0300

    update

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,32 +1,10 @@
-const verydisco = word =>{
-    const wordArr = word.split("")
-    let count = Math.ceil(word.length/2)
-    let i = 0
-    let w1 = ""
-    let w2 = ""
-
-    wordArr.forEach(letter =>{
-        if (i < count){
-            w1 += String(letter)
-        }else{
-            w2 += String(letter)
-        }
-        i++      
-    })
-    return w2+w1
-}
-const word = process.argv[2]
-let answer = ""
-
-if (word.includes(" ")){
-    let answer1 = []
-    const wordArr = word.split(" ")
-    wordArr.forEach(element=> {
-        answer1.push(verydisco(element))
-    })
-    answer = answer1.join(' ')
-}else{
-    answer = verydisco(word)
+const verydisco = (word) =>{
+    const count = Math.ceil(word.length/2)
+    return word.slice(count) + word.slice(0, count)
 }
 
-console.log(answer)
+const verydiscoInput = (input) => input.includes(" ") ? input.split(" ").map(element => verydisco(element)).join(" ") : verydisco(input)
+
+let wordInput = process.argv[2]
+
+console.log(verydiscoInput(wordInput))
